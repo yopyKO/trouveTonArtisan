a6a6075c72ce1e954c2e5a5e868ccd6ed591b84b
CharlyGr <charly.grosbois@gmail.com>
2024-11-19 17:15:41 +0100

    add 2nd modif

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as arti from "../list/datas.json";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  artisans: any = (arti as any).default;
 }
